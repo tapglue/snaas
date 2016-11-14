df54eb8c7d51dba313ed242310434e97adabbf7c
Alexander Simmerl <xla@tapglue.com>
2016-11-14 14:37:43 +0100

    Change default AMIs

--- infrastructure/terraform/template/variables.tf
+++ infrastructure/terraform/template/variables.tf
@@ -19,12 +19,12 @@ variable "ami_ecs_agent" {
 
 variable "ami_minimal" {
   default = {
-    "us-east-1"     = "ami-6d1c2007"
-    "us-east-2"     = "ami-6a2d760f"
-    "us-west-1"     = "ami-af4333cf"
-    "us-west-2"     = "ami-d2c924b2"
-    "eu-west-1"     = "ami-7abd0209"
-    "eu-central-1"  = "ami-9bf712f4"
+    "us-east-1"     = "ami-1924770e"
+    "us-east-2"     = "ami-bd3e64d8"
+    "us-west-1"     = "ami-7f004b1f"
+    "us-west-2"     = "ami-56ed4936"
+    "eu-west-1"     = "ami-c8337dbb"
+    "eu-central-1"  = "ami-dd12ebb2"
   }
   description = "AMIs used for auxiliary hosts"
   type        = "map"
