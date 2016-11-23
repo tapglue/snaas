4fc137c4259af9914e605c02046b989292a19da3
Alexander Hellbom <alexander.hellbom@gmail.com>
2016-11-23 15:28:47 +0100

    Bump service versions

--- infrastructure/terraform/template/variables.tf
+++ infrastructure/terraform/template/variables.tf
@@ -6,47 +6,50 @@ variable "account" {
 
 variable "ami_ecs_agent" {
   default = {
-    "us-east-1"     = "ami-1924770e"
-    "us-east-2"     = "ami-bd3e64d8"
-    "us-west-1"     = "ami-7f004b1f"
-    "us-west-2"     = "ami-56ed4936"
-    "eu-west-1"     = "ami-c8337dbb"
-    "eu-central-1"  = "ami-dd12ebb2"
+    "us-east-1"    = "ami-1924770e"
+    "us-east-2"    = "ami-bd3e64d8"
+    "us-west-1"    = "ami-7f004b1f"
+    "us-west-2"    = "ami-56ed4936"
+    "eu-west-1"    = "ami-c8337dbb"
+    "eu-central-1" = "ami-dd12ebb2"
   }
+
   description = "AMIs used for ecs agent"
   type        = "map"
 }
 
 variable "ami_minimal" {
   default = {
-    "us-east-1"     = "ami-1924770e"
-    "us-east-2"     = "ami-bd3e64d8"
-    "us-west-1"     = "ami-7f004b1f"
-    "us-west-2"     = "ami-56ed4936"
-    "eu-west-1"     = "ami-c8337dbb"
-    "eu-central-1"  = "ami-dd12ebb2"
+    "us-east-1"    = "ami-1924770e"
+    "us-east-2"    = "ami-bd3e64d8"
+    "us-west-1"    = "ami-7f004b1f"
+    "us-west-2"    = "ami-56ed4936"
+    "eu-west-1"    = "ami-c8337dbb"
+    "eu-central-1" = "ami-dd12ebb2"
   }
+
   description = "AMIs used for auxiliary hosts"
   type        = "map"
 }
 
 variable "elb_id" {
-  default     = {
-    "us-east-1"       = "127311923021"
-    "us-east-2"       = "033677994240"
-    "us-west-1"       = "027434742980"
-    "us-west-2"       = "797873946194"
-    "eu-west-1"       = "156460612806"
-    "eu-central-1"    = "054676820928"
-    "ap-northeast-1"  = "582318560864"
-    "ap-northeast-2"  = "600734575887"
-    "ap-southeast-1"  = "114774131450"
-    "ap-southeast-2"  = "783225319266"
-    "ap-south-1"      = "718504428378"
-    "sa-east-1"       = "507241528517"
-    "us-gov-west-1"   = "048591011584"
-    "cn-north-1"      = "638102146993"
+  default = {
+    "us-east-1"      = "127311923021"
+    "us-east-2"      = "033677994240"
+    "us-west-1"      = "027434742980"
+    "us-west-2"      = "797873946194"
+    "eu-west-1"      = "156460612806"
+    "eu-central-1"   = "054676820928"
+    "ap-northeast-1" = "582318560864"
+    "ap-northeast-2" = "600734575887"
+    "ap-southeast-1" = "114774131450"
+    "ap-southeast-2" = "783225319266"
+    "ap-south-1"     = "718504428378"
+    "sa-east-1"      = "507241528517"
+    "us-gov-west-1"  = "048591011584"
+    "cn-north-1"     = "638102146993"
   }
+
   description = "Mapping of ELB account IDs needed to enable log access on S3 bucket"
   type        = "map"
 }
@@ -61,6 +64,7 @@ variable "key" {
   default = {
     "access" = ""
   }
+
   description = "SSH public keys"
   type        = "map"
 }
@@ -90,10 +94,11 @@ variable "pg_password" {
 }
 
 variable "version" {
-  default     = {
-    "gateway-http" = "90"
-    "sims" = "90"
+  default = {
+    "gateway-http" = "95"
+    "sims"         = "95"
   }
+
   description = "Versions used for deployed services"
   type        = "map"
 }
