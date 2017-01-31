4596f969b294b60f603013de1c57b77988e33c83
Alexander Simmerl <xla@tapglue.com>
2017-01-31 13:25:45 +0100

    Create reaction queue

--- infrastructure/terraform/template/storage.tf
+++ infrastructure/terraform/template/storage.tf
@@ -265,6 +265,32 @@ EOF
   visibility_timeout_seconds = 60
 }
 
+resource "aws_sqs_queue" "reaction-state-change-dlq" {
+  delay_seconds              = 0
+  max_message_size           = 262144
+  message_retention_seconds  = 1209600
+  name                       = "reaction-state-change-dlq"
+  receive_wait_time_seconds  = 1
+  visibility_timeout_seconds = 300
+}
+
+resource "aws_sqs_queue" "reaction-state-change" {
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 1209600
+  name                      = "reaction-state-change"
+  receive_wait_time_seconds = 1
+
+  redrive_policy = <<EOF
+{
+    "deadLetterTargetArn": "${aws_sqs_queue.reaction-state-change-dlq.arn}",
+    "maxReceiveCount": 10
+}
+EOF
+
+  visibility_timeout_seconds = 60
+}
+
 # Device update queues, topics and subscriptions.
 resource "aws_sqs_queue" "endpoint-state-change-dlq" {
   delay_seconds              = 0
